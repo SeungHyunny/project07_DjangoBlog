04eea4b3f9a72a9f66f71a570dfc95efade688ba
SeungHyun <90026605+SeungHyunny@users.noreply.github.com>
2021-10-12 15:58:45 +0900

    Add files via upload

---
+++ bookmark/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from django.views.generic import ListView,DetailView,TemplateView 
+from bookmark.models import Bookmark 
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from mysite.views import OwnerOnlyMixin
+
+class BookmarkLV(ListView):
+    model = Bookmark
+    template_name = 'bookmark/bookmark_list.html'
+
+class BookmarkDV(DetailView):
+    model = Bookmark
+    template_name = 'bookmark/bookmark_detail.html'
+
+class BookmarkCreateView(LoginRequiredMixin, CreateView):
+    model = Bookmark
+    fields = ['title','url']
+    success_url = reverse_lazy('bookmark:index')
+    
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+    
+class BookmarkChangeLV(LoginRequiredMixin, ListView):
+    template_name = 'bookmark/bookmark_change_list.html'
+    
+    def get_queryset(self):
+        return Bookmark.objects.filter(owner=self.request.user)
+    
+class BookmarkUpdateView(OwnerOnlyMixin, UpdateView):
+    model = Bookmark
+    fields = ['title','url']
+    success_url = reverse_lazy('bookmark:index')
+    
+class BookmarkDeleteView(OwnerOnlyMixin, DeleteView):
+    model = Bookmark
+    success_url = reverse_lazy('bookmark:index')
